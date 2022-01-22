070b11fdb380e8c4987b3d130bb1f657811f408e
Vitor Nathan Goncalves <vitornathang@gmail.com>
2022-01-22 15:35:42 -0300

    Changed root image to Ubuntu and added .gitignore

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,24 +1,23 @@
 package com.example;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.time.Instant;
 
 public class App {
 
-
     public static void main(String[] args) throws InterruptedException, IOException {
         File file = new File("logs/teste.log");
-        if(!file.exists()) {
+        if (!file.exists()) {
             file.createNewFile();
         }
         System.setOut(new PrintStream(file));
         while (true) {
             final long sleepTime = Math.round(Math.random() * 2000);
             Thread.sleep(sleepTime);
-            System.out.println("{\"message\": \"Hello World! Slept by: " + sleepTime + "\", \"timestamp\": " + Instant.now().toEpochMilli() +"}");
+            System.out.println("{\"message\": \"Hello World! Slept by: " + sleepTime + "\", \"timestamp\": "
+                    + Instant.now().toEpochMilli() + "}");
         }
     }
 }
